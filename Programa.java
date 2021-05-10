ca3704f62d9466b4dc27531b060b3272b0e93701
Estefania Lopez <estefania.lopezv@upb.edu.co>
2021-05-09 20:35:50 -0500

    se añadío proceso de acentos

--- Programa.java
+++ Programa.java
@@ -1,6 +1,139 @@
 import java.util.Scanner;
+import org.fusesource.jansi.AnsiConsole;
+import static org.fusesource.jansi.Ansi.*;
+import static org.fusesource.jansi.Ansi.Color.*;
 
-public class Programa {
+public class Programa{
+
+	public static void displayVacio(int n,int m){
+		for (int i=0;i<n;i++) 
+		{
+			for (int j=0;j<m;j++)
+			{
+				System.out.print("  ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void imprimirDisplay(int n,int m){
+		String simbolo = Math.random()>5?"*":" ";
+		for (int i=0;i<n;i++) 
+		{
+			if(i+1==n)
+			{
+				simbolo = "#";
+
+			}
+			for (int j=0;j<m;j++)
+			{
+				System.out.print(i+simbolo);
+			}
+			System.out.println();
+		}
+	}
+
+	public static void borrarDisplay(int n){
+		//Se le suma 1 si se ejecuta con el bat, ya que la ultima linea es el pause
+		//for (int i=0;i<=n;i++){
+			//System.out.println(ansi().cursorUpLine(i));
+		//}
+		System.out.println(ansi().cursor(2,1));
+		displayVacio(5,6);
+		System.out.println(ansi().cursor(1,1));
+		System.out.println(ansi().reset());
+	}
+
+	public static void animar() throws InterruptedException{
+		
+
+		Thread.sleep(1000);
+		
+		/*
+		for(int i=0;i<10;i++){
+			System.out.print("\r");
+			//System.out.print("\r");
+			texto = texto+nuevo.charAt(i);
+			System.out.print(texto);
+			Thread.sleep(1000);
+		}*/
+	}
+
+	public static String convertirUnicode(String letra,String cadena){
+		
+		StringBuilder str = new StringBuilder(cadena);
+		int indice = -1;
+		char caracter = 0;
+		indice = str.lastIndexOf(letra);
+		
+		if(indice>=0 ){
+			switch(letra){
+				case "á": caracter = '\u00E1';
+					      break;
+				case "é": caracter = '\u00E9';
+					      break;
+				case "í": caracter = '\u00ED';
+					      break;
+		        case "ó": caracter = '\u00F3';
+					      break;
+			    case "ú": caracter = '\u00FA';
+					      break;
+				case "ñ": caracter = '\u00F1';
+					      break;
+				case "Á": caracter = '\u00C1';
+						  break;
+				case "É": caracter = '\u00C9';
+						  break;
+				case "Í": caracter = '\u00CD';
+						  break;
+				case "Ó": caracter = '\u00D3';
+						  break;
+				case "Ú": caracter = '\u00DA';
+						  break;
+				case "Ñ": caracter = '\u00D1';
+						  break;
+			}
+			// System.out.println("\\u" + Integer.toHexString('÷' | 0x10000).substring(1));
+			str.replace(indice,indice+1,""+caracter);
+		}
+
+		return str.toString();
+	}
+
+	public static void imprimir(String cadena){
+		String str; 
+		str = convertirUnicode("á",cadena);
+		str = convertirUnicode("Á",cadena);
+		str = convertirUnicode("á",str);
+		str = convertirUnicode("é",str);
+		str = convertirUnicode("í",str);
+		str = convertirUnicode("ó",str);
+		str = convertirUnicode("ú",str);
+		str = convertirUnicode("ñ",str);
+		str = convertirUnicode("É",str);
+		str = convertirUnicode("Í",str);
+		str = convertirUnicode("Ó",str);
+		str = convertirUnicode("Ú",str);
+		str = convertirUnicode("Ñ",str);
+
+		System.out.println(str);
+	}
+
+	public static StringBuilder obtenerLetraCancion(int inicio,int fin, String[]data){
+		StringBuilder str = new StringBuilder();
+
+		for(int i = inicio; i<=fin; i++)
+		{
+			str.append(data[i]+"\n");		
+		}
+		/*for (int j = 0; j <= str.length()-1; j ++){
+			if(str(j).equals("\u003B")){
+				str.replace("\u003B", "\u0020");
+			}			
+		}*/
+		return str;
+	}
+
     public static void main(String[] args) {
         Scanner dato = new Scanner(System.in);
         int opcionMenuPrincipal;
